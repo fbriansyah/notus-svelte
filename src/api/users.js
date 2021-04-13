953fa335502a90b338ec3e1a55c1bc1a1794eec7
Febriansyah <febrian@efner.id>
2021-04-13 08:28:44 +0700

    add user editor setting

--- src/api/users.js
+++ src/api/users.js
@@ -85,3 +85,81 @@ export function filterFields() {
     },
   ]
 }
+
+export function editorFields() {
+  return [
+    {
+      kind: 'input',
+      name: 'username',
+      label: 'Username',
+      options: {
+        type: 'text',
+        prefix: "workcation.com/",
+        placeholder: ''
+      },
+    },
+    {
+      kind: 'input',
+      name: 'fullname',
+      label: 'Fullname',
+      options: {
+        type: 'text',
+        prefix: "",
+        placeholder: "Nama Lengkap"
+      },
+    },
+    {
+      kind: 'text-area',
+      name: 'about',
+      label: 'About',
+      options: {
+        rows: 3,
+        helperText:"Write a few sentences about yourself."
+      },
+    },
+    {
+      kind: 'checkbox',
+      name: 'comments',
+      label: 'By Email',
+      items: [
+        {
+          label: "Comments",
+          value: "comments",
+          description: "Get notified when someones posts a comment on a posting.",
+        },
+        {
+          label: "Candidates",
+          value: "candidates",
+          description: "Get notified when a candidate applies for a job.",
+        },
+        {
+          label: "Offers",
+          value: "offers",
+          description: "Get notified when a candidate accepts or rejects an offer.",
+        },
+      ]
+    },
+    {
+      kind: 'radio',
+      name: 'push-notification',
+      label: 'Push Notifications',
+      description: "These are delivered via SMS to your mobile phone.",
+      items: [
+        { label: "Everything", value: 1 },
+        { label: "Same as email", value: 2 },
+        { label: "No push notifications", value: 3 },
+      ]
+    },
+    {
+      kind: 'select',
+      name: 'level',
+      label: 'Level',
+      description: "These are delivered via SMS to your mobile phone.",
+      items: [
+        { label: "User", value: 1 },
+        { label: "Admin", value: 2 },
+        { label: "Editor", value: 3 },
+      ]
+    }
+  ]
+}
